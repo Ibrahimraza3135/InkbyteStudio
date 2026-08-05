f1d3c8a13a5f3a474065d930ecb10f5bbafc5025
Ibrahimraza3135 <ibrahimraza3135@gmail.com>
2026-08-05 12:26:29 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -68,9 +68,7 @@ export default function Footer({ onNavigate }: FooterProps) {
               </a>
             </p>
 
-            <p className="text-sm mt-2">
-              <strong>Office:</strong> Manchester, United Kingdom
-            </p>
+           
             <p className="text-sm">Mon – Fri: 10 AM – 6 PM</p>
           </div>
         </div>
